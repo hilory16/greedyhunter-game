9cb8c2c3cfecad53e521f423979c279b4c488073
Emmanuel Ilori <hiloryolamide@gmail.com>
2021-04-03 13:16:14 +0100

    Move barrier

--- src/pages/GamePlay/index.js
+++ src/pages/GamePlay/index.js
@@ -8,19 +8,27 @@ import './style.scss';
 export default class Index extends Component {
 
     state={
-        gridNumber:100,
+        gridNumber:"",
         food:10,
+        grid:10,
         foodLocation:[],
         playerPosition:'',
         playerSelected:false,
         playerError:false,
-        validGrids:[]
+        validGrids:[],
+        totalMoves:0,
+        topMoveBarrier:[],
+        bottomMoveBarrier:[],
+        leftMoveBarrier:[],
+        rigthMoveBarrier:[]
     }
 
     componentDidMount(){
-        console.log(this.placeFood())
+        // console.log(this.placeFood())
+        this.setMoveBarrier()
         this.setState({
-            foodLocation:this.placeFood()
+            foodLocation:this.placeFood(),
+            gridNumber:this.state.grid * this.state.grid
         })
     }
 
@@ -38,16 +46,19 @@ export default class Index extends Component {
         // 
     }
 
-    selectFood = (target) =>{
-        if(this.state.playerSelected){
+    MovePlayer = (target) =>{
+        const {playerSelected, foodLocation, totalMoves} =  this.state
+        if(playerSelected){
             this.setState({playerError:false})
 
             const foods = this.state.foodLocation
             const exist = foods.includes(target);
-            if(!exist && !this.state.validGrids.includes(target)) return null
+            if(!this.state.validGrids.includes(target)) return null
+            if(!exist) return this.setState({playerPosition:target, totalMoves: totalMoves + 1},this.selectPlayer("auto",target))
 
-            var removed = foods.splice(this.state.foodLocation.indexOf(target),1);
-            this.setState({foodLocation:foods,playerPosition:target},this.selectPlayer())
+            var removed = foods.splice(foodLocation.indexOf(target),1);
+            this.setState({foodLocation:foods,playerPosition:target, totalMoves: totalMoves + 1},this.selectPlayer("auto",target))
+            
             
             
 
@@ -55,17 +66,105 @@ export default class Index extends Component {
             this.setState({playerError:true})
         }
     }
+    // playerSelected ? activeFields : []playerSelected ? activeFields : []
 
-    selectPlayer = () =>{
-        const {playerPosition, food,playerSelected} = this.state
-        this.setState({playerSelected:!playerSelected})
+    selectPlayer = (type,target) =>{
+        const {playerPosition, food,playerSelected, topMoveBarrier, bottomMoveBarrier, leftMoveBarrier, rightMoveBarrier} = this.state
+        this.setState({playerSelected:type === "auto" ? true : !playerSelected})
+
+        const activePosition = type === "auto" ? target  : playerPosition
+        let activeBarrier = ''
+        if(topMoveBarrier.includes(activePosition)){
+            const index = topMoveBarrier.indexOf(activePosition)
+            index === 0 ? activeBarrier = 'top-left': index === topMoveBarrier.length - 1 ? activeBarrier = 'top-right' :  activeBarrier = 'top'
+        }else if(bottomMoveBarrier.includes(activePosition)){
+            const index = bottomMoveBarrier.indexOf(activePosition)
+            index === 0 ? activeBarrier = 'bottom-left': index === bottomMoveBarrier.length - 1 ? activeBarrier = 'bottom-right' :  activeBarrier = 'bottom'
+        }else if(leftMoveBarrier.includes(activePosition)){
+            activeBarrier = 'left'
+        }else if(rightMoveBarrier.includes(activePosition)){
+            activeBarrier = 'right'
+        }
+        console.log(topMoveBarrier, bottomMoveBarrier, leftMoveBarrier, rightMoveBarrier)
+
+        
+
         const activeFields = []
-        const top = playerPosition - food
-        const bottom = playerPosition + food
-        activeFields.push(top-1,top, top + 1, playerPosition -1, playerPosition + 1, bottom -1, bottom, bottom + 1)
-        this.setState({validGrids:activeFields})
-        console.log(playerPosition, food,playerSelected)
+        const top = activePosition - food
+        const bottom = activePosition + food
+        switch(activeBarrier){
+            case "top":
+                activeFields.push(activePosition -1, activePosition + 1, bottom -1, bottom, bottom + 1);
+                break;
+            case "top-left":
+                activeFields.push(activePosition + 1,bottom, bottom + 1);
+                break;
+            case "top-right":
+                activeFields.push(activePosition -1, bottom -1, bottom);
+                break;
+            case "bottom":
+                activeFields.push(top - 1,top, top + 1, activePosition -1, activePosition + 1)
+                break;
+            case "bottom-left":
+                activeFields.push(top, top + 1, activePosition + 1);
+                break;
+            case "bottom-right":
+                activeFields.push(top - 1,top, activePosition -1);
+                break;
+            case "left":
+                activeFields.push(top, top + 1, activePosition + 1,bottom, bottom + 1)
+                break;
+            case "right":
+                activeFields.push(top - 1,top,activePosition -1,bottom -1, bottom)
+                break;
+            default:
+                activeFields.push(top - 1,top, top + 1, activePosition -1, activePosition + 1, bottom -1, bottom, bottom + 1)
+        }
+        
+        this.setState({validGrids:type === "auto" ?  activeFields : !playerSelected ? activeFields : []})
+        // console.log(playerSelected)
+        
     }
+
+    setMoveBarrier = () =>{
+        let array = []
+        const {grid} = this.state
+
+        //SET TOP MOVE BARRIER
+        for(let i = 1; i <= grid; i++){
+            array.push(i)
+        }
+        this.setState({topMoveBarrier:array})
+        array = []
+
+        //SET BOTTOM MOVE BARRIER
+        for(let i = (grid * grid) - grid + 1; i <= (grid * grid); i++){
+            array.push(i)
+        }
+        this.setState({bottomMoveBarrier:array})
+        array = []
+
+        //SET LEFT MOVE BARRIER
+        for(let i = 1; i <= grid - 2; i++){
+            array.push((grid * i) + 1)
+        }
+        this.setState({leftMoveBarrier:array})
+        array = []
+        
+        //SET RIGHT MOVE BARRIER
+        for(let i = 2; i <= grid - 1; i++){
+            array.push(grid * i)
+        }
+        this.setState({rightMoveBarrier:array})
+
+        
+
+        // console.log(array)
+
+
+        
+    }
+
     renderBox = () =>{
         const array = []
         for(let i = 1; i <= this.state.gridNumber; i++){
@@ -73,12 +172,12 @@ export default class Index extends Component {
         }
         return array.map((item, i) =>{
             
-            if(this.state.playerPosition == i) return<div className={`board__box d-flex align-items-center justify-content-center ${this.state.playerError ? 'player__error' : ''} ${this.state.playerSelected ? 'player__selected' : ''}`} onClick={() =>  this.selectPlayer()}>
+            if(this.state.playerPosition == (i + 1)) return<div className={`board__box d-flex align-items-center justify-content-center ${this.state.playerError ? 'player__error' : ''} ${this.state.playerSelected ? 'player__selected' : ''}`} onClick={() =>  this.selectPlayer()}>
                 <img src={Character} alt="" className={`character__play`}/>
             </div>
             return(
-                <div className={`board__box d-flex align-items-center justify-content-center ${this.state.playerSelected ? 'player__active' : ''} ${this.state.validGrids.includes(i) ? 'player__possible' : ''}`} onClick={() =>this.selectFood(i)}>
-                    <img src={Food} alt="" className={`${this.state.foodLocation.includes(i) ? '' : 'd-none'}`}/>
+                <div className={`board__box d-flex align-items-center justify-content-center ${this.state.playerSelected ? 'player__active' : ''} ${this.state.validGrids.includes(i + 1) ? 'player__possible' : ''}`} onClick={() =>this.MovePlayer(i + 1)}>
+                    <img src={Food} alt="" className={`${this.state.foodLocation.includes(i + 1) ? '' : 'd-none'}`}/>
                     {
                         // <img src={Food} alt="" className={`${this.state.foodLocation.includes(i) ? '' : 'd-none'}`}/>
                         // <img src={Food} alt="" className={`${this.state.foodLocation.includes(i) ? '' : 'd-none'}`}/>
@@ -91,6 +190,8 @@ export default class Index extends Component {
     }
     render() {
         // console.log(this.state.validGrids)
+        const {playerPosition, food,playerSelected, validGrids, foodLocation, gridNumber, grid, totalMoves} = this.state
+        // console.log(playerPosition, validGrids,playerSelected, foodLocation)
         return (
             <section className="gameplay d-flex align-items-center justify-content-center">
                 <div className="gameboard">
@@ -108,6 +209,11 @@ export default class Index extends Component {
                     </div>
                     <div className="board__item ">
                         {this.renderBox()}
+                    </div>
+
+                    <div className="meta d-flex jsutify-content-evenly align-items-center">
+                        <div className="max__moves data pl-3">Maximum moves: <span>{Math.ceil(grid * grid / 2)} </span></div>
+                        <div className="total__moves data pr-3">Total moves: <span>{totalMoves}</span></div>
                     </div>
                 </div>
             </section>
